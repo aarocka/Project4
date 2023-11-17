21bce33ea4d0ac1f975615dcc418c76faae56467
Aaron Kolker <aarocka@users.noreply.github.com>
2023-11-17 18:28:32 -0500

    fixed reservation update

--- RestaurantReviewProject3/EditReservation.aspx.cs
+++ RestaurantReviewProject3/EditReservation.aspx.cs
@@ -2,11 +2,18 @@
 using System.Data;
 using System.Data.SqlClient;
 using Utilities;
+using RestaurantReviewLibrary;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Net;
 
 namespace RestaurantReviewProject3
 {
     public partial class EditReservation : System.Web.UI.Page
     {
+        String webApiUrl = "http://localhost:5054/api/management/update/";
+        //reservation
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -19,6 +26,19 @@ namespace RestaurantReviewProject3
             String update = (String)Session["update"];
             Session["update"] = update;
 
+            //Create a new reservation object
+            Reservation reservation = new Reservation();
+            reservation.Name = TextBox1.Text;
+            reservation.Date = Calendar1.SelectedDate.ToString("yyyy-MM-dd");
+            reservation.Time = DropDownList1.SelectedValue.ToString();
+            reservation.Id = int.Parse(update);
+
+            //Serialize the reservation object to JSON format
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            String jsonReservation = js.Serialize(reservation);
+
+
+            /*
             DBConnect dB = new DBConnect();
             SqlCommand sql = new SqlCommand();
             sql.CommandText = "ChangeReservation";
@@ -28,8 +48,49 @@ namespace RestaurantReviewProject3
             sql.Parameters.AddWithValue("@Date", Calendar1.SelectedDate.ToString("yyyy-MM-dd"));
             sql.Parameters.AddWithValue("@Time", DropDownList1.SelectedValue);
             dB.DoUpdateUsingCmdObj(sql);
+            */
 
-            Response.Redirect("RepresentativePage.aspx");
+            try
+            {
+                // Send the Customer object to the Web API that will be used to store a new customer record in the database.
+                // Setup an HTTP POST Web Request and get the HTTP Web Response from the server.
+                WebRequest request = WebRequest.Create(webApiUrl + "reservation/");
+                request.Method = "PUT";
+                request.ContentLength = jsonReservation.Length;
+                request.ContentType = "application/json";
+
+                // Write the JSON data to the Web Request
+                StreamWriter writer = new StreamWriter(request.GetRequestStream());
+                writer.Write(jsonReservation);
+                writer.Flush();
+                writer.Close();
+
+                // Read the data from the Web Response, which requires working with streams
+                WebResponse response = request.GetResponse();
+                Stream theDataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(theDataStream);
+                String data = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+
+                if (data == "true")
+                {
+                    //Label3.Text = "The customer was successfully saved to the database.";
+                    //Label3.Visible = true;
+                    //Button1.Visible = false;
+                    Response.Redirect("RepresentativePage.aspx");
+                }
+                else
+                {
+                    //Label3.Text = "A problem occurred while adding the customer to the database. The data wasn't recorded.";
+                }
+            }
+            catch (Exception ex)
+            {
+                //Label3.Text = "Error: " + ex.Message;
+            }
+
+            
         }
     }
 }
