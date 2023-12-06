1b2728834016bacbea438c129d8b76010f1dec47
Aaron Kolker <aarocka@users.noreply.github.com>
2023-12-06 17:41:45 -0500

    got basic restaurant list working

--- CoreSite/Controllers/RestaurantsController.cs
+++ CoreSite/Controllers/RestaurantsController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RestaurantReviewLibrary;
+using System.Net;
+using System.IO;
+using System.Text.Json;
 
 namespace CoreSite.Controllers
 {
@@ -9,13 +12,35 @@ namespace CoreSite.Controllers
         // GET: RestaurantsController
         public ActionResult Index()
         {
-
+            /*
             List<Restaurant> restaurants = new List<Restaurant>();
 
             restaurants.Add(new Restaurant(1, "a", "cat1", "123 st", "123-233-4444", "example.com"));
             restaurants.Add(new Restaurant(2, "b", "cat2", "123 st", "123-233-4444", "example.com"));
             restaurants.Add(new Restaurant(3, "c", "cat3", "123 st", "123-233-4444", "example.com"));
+            */
 
+            //make a get request to the api
+            string webApiUrl = "http://localhost:5054/api/restaurant/all";
+            WebRequest request = WebRequest.Create(webApiUrl);
+            WebResponse response = request.GetResponse();
+
+            //read the data from the web response, which requires working with streams
+            System.IO.Stream theDataStream = response.GetResponseStream();
+            System.IO.StreamReader reader = new System.IO.StreamReader(theDataStream);
+            string data = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+
+            //deserialize a json string into a list<restaurant>
+            Console.WriteLine(data);
+
+
+            List<Restaurant> restaurants = JsonSerializer.Deserialize<List<Restaurant>>(data,new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+            
 
             return View(restaurants);
         }
