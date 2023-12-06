237c3a48435870e219c95039cfc3b3647b94e050
Aaron Kolker <aarocka@users.noreply.github.com>
2023-12-06 15:25:53 -0500

    added core site

---
+++ CoreSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
