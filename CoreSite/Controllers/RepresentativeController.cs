0b440627632bf56b70f97e8550d63077c1d6e904
Aaron Kolker <aarocka@users.noreply.github.com>
2023-12-08 16:43:35 -0500

    Got update Restraunt info working

--- CoreSite/Controllers/RepresentativeController.cs
+++ CoreSite/Controllers/RepresentativeController.cs
@@ -157,5 +157,102 @@ namespace CoreSite.Controllers
                 return View();
             }
         }
+
+        // GET: RepresentativeController/UpdadeInfo
+        public ActionResult UpdateInfo()
+        {
+            //check if session is null
+            if (HttpContext.Session.GetString("UserSession") == null)
+            {
+                //user is not logged in
+                //Redirect to login page
+                return Redirect("http://example.com");
+            }
+            else
+            {
+                //User is logged in
+                //Deserialize HttpContext.Session.GetString("UserSession");
+                UserSession userSession = JsonSerializer.Deserialize<UserSession>(HttpContext.Session.GetString("UserSession"), new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    IncludeFields = true
+                });
+
+                //get restaurant from api
+                //make a get request to the api
+                string webApiUrl = "http://localhost:5054/api/restaurant/"+userSession.RestaurantID;
+
+                //make a get request to the api
+                WebRequest request = WebRequest.Create(webApiUrl);
+                WebResponse response = request.GetResponse();
+
+                //read the data from the web response, which requires working with streams
+                System.IO.Stream theDataStream = response.GetResponseStream();
+                System.IO.StreamReader reader = new System.IO.StreamReader(theDataStream);
+                string data = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+
+                //deserialize the json data
+                Restaurant restaurant = JsonSerializer.Deserialize<Restaurant>(data, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    IncludeFields = true
+                });
+
+                return View(restaurant);
+            }
+        }
+
+        // POST: RepresentativeController/UpdadeInfo
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateInfo(Restaurant restaurant)
+        {
+            //check if session is null
+            if (HttpContext.Session.GetString("UserSession") == null)
+            {
+                //user is not logged in
+                //Redirect to login page
+                return Redirect("http://example.com");
+            }
+            else
+            {
+                //User is logged in
+                //Deserialize HttpContext.Session.GetString("UserSession");
+                UserSession userSession = JsonSerializer.Deserialize<UserSession>(HttpContext.Session.GetString("UserSession"), new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    IncludeFields = true
+                });
+
+                //update restaurant in api
+                //Serialize the restaurant object to JSON format
+                string json = JsonSerializer.Serialize(restaurant);
+                 
+                //make api call to update restaurant
+                string webApiUrl = "http://localhost:5054/api/management/update/restaurant";
+                WebRequest request = WebRequest.Create(webApiUrl);
+                request.Method = "PUT";
+                request.ContentLength = json.Length;
+                request.ContentType = "application/json";
+
+                // Write the JSON data to the Web Request
+                StreamWriter writer = new StreamWriter(request.GetRequestStream());
+                writer.Write(json);
+                writer.Flush();
+                writer.Close();
+
+                WebResponse response = request.GetResponse();
+                Stream theDataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(theDataStream);
+                string data = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+
+                //Redirect to index
+                return RedirectToAction(nameof(Index));
+            }
+        }
     }
 }
